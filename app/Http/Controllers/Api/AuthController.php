034501589d0c67c72f9d8570c1bab72fc1a85c6a
Raayhan <rayhan.rakib@northsouth.edu>
2023-06-24 11:43:15 +0600

    Signup function in AuthControlller

--- app/Http/Controllers/Api/AuthController.php
+++ app/Http/Controllers/Api/AuthController.php
@@ -7,16 +7,30 @@ use Illuminate\Http\Request;
 
 class AuthController extends Controller
 {
-    public function login(){
+  
+    public function signup(SignupRequest $request){
+
+        $data = $request->validated();
+        $user = User::create([
+            'name' => $data['name'],
+            'email' => $data['email'],
+            'password' => bcrypt($data['password']),
+        ]);
+
+        $token = $user->createToken('main')->plainTextToken;
+
+        return response(compact('user','token'));
 
     }
 
-    public function signup(){
+    public function login(LoginRequest $request){
+
+       
 
     }
 
-    public function logout(){
+    public function logout(Request $request){
 
-        
+
     }
 }

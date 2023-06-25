289de32fc28c5c6fd3e894b4b9945ed6dbe41ad2
Raayhan <rayhan.rakib@northsouth.edu>
2023-06-25 15:55:53 +0600

    Client side request for get all users

--- react-frontend/src/views/Users.jsx
+++ react-frontend/src/views/Users.jsx
@@ -1,5 +1,28 @@
+import { useEffect, useState } from "react"
+import axiosClient from "../axios.client";
+
 export default function Users() {
 
+    const [users, setUser] = useState([]);
+    const [loading, setLoading] = useState(false);
+
+    useEffect(() => {
+        getUsers();
+
+    }, [])
+
+    const getUsers = () => {
+        setLoading(true);
+        axiosClient.get('/users')
+            .then(({ data }) => {
+                setLoading(false);
+                console.log(data);
+
+            })
+            .catch(() => {
+                setLoading(false);
+            })
+    }
     return (
 
         <div>
